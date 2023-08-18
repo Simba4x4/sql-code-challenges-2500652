ad5e24408d3ea8b97462d15adede8e66cd228eeb
Simba4x4 <142573072+Simba4x4@users.noreply.github.com>
2023-08-18 08:49:41 +0000

    update

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,9 @@
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
+
+Select * from Kunden where Vorname='Taylor' and Nachname = 'Jenkins' and Adresse = '27170 6th Ave.';
+
+UPDATE Kunden 
+Set Adresse = '74 Pine St.', Stadt = 'New York', State = 'NY' 
+Where Vorname = 'Taylor' and Nachname = 'Jenkins' and Adresse = '27170 6th Ave';
