d4780ef3b9076180246eeb94212b3234e9d5ea47
Simba4x4 <142573072+Simba4x4@users.noreply.github.com>
2023-08-18 09:25:59 +0000

    update

--- 01_restaurant/challenges/01_06_loescheeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_loescheeinfo_ch.sql
@@ -2,3 +2,10 @@
 
 -- Ein Kunde namens Norby hat uns informiert, dass er seine Reservierung
 -- für Freitag stornieren moechte. Heute ist der 24.Juli 2022.
+
+Select * From Reservierung 
+Join Kunden on Reservierung.KundenID = Kunden.KundenID
+where Kunden.Vorname = 'Norby'
+And Reservierung.Datum > '2022-07-24';
+
+Delete From Reservierung where ReservierungsID = 2000;
