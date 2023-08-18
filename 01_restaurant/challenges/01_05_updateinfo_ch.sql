43e2879ce37635a063a2f8d66547c1ae0efccaf8
Simba4x4 <142573072+Simba4x4@users.noreply.github.com>
2023-08-18 08:53:23 +0000

    update

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -6,5 +6,5 @@
 Select * from Kunden where Vorname='Taylor' and Nachname = 'Jenkins';
 
 UPDATE Kunden 
-Set Adresse = '75 Pine St.', Stadt = 'New York', State = 'NY' 
-Where Vorname = 'Taylor' and Nachname = 'Jenkins' and Adresse = '74 Pine St.';
+Set Adresse = '74 Pine St.', Stadt = 'New York', State = 'NY' 
+Where Vorname = 'Taylor' and Nachname = 'Jenkins' and Adresse = '75 Pine St.';
