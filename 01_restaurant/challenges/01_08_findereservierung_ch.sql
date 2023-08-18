db2ae248d01fb17f99497025cc954ec5409c5ee0
Simba4x4 <142573072+Simba4x4@users.noreply.github.com>
2023-08-18 11:58:58 +0000

    upödate

--- 01_restaurant/challenges/01_08_findereservierung_ch.sql
+++ 01_restaurant/challenges/01_08_findereservierung_ch.sql
@@ -4,3 +4,8 @@
 -- Der Name hat unter anderem folgenede Variationen:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 -- Heute ist der 14.Juni.
+
+Select Kunden.Nachname, Datum, PartyGroesse 
+from Reservierung 
+Join Kunden on Kunden.KundenID = Reservierung.KundenID
+where Datum Like '2022-06-14%' and Kunden.Nachname Like 'St%';
