7f12ba04117dd4a0edfbb6f4b4fbd57307605d33
Simba4x4 <142573072+Simba4x4@users.noreply.github.com>
2023-08-18 11:31:45 +0000

    update

--- 01_restaurant/challenges/01_07_teilnahmeevent_ch.sql
+++ 01_restaurant/challenges/01_07_teilnahmeevent_ch.sql
@@ -1,3 +1,12 @@
 -- Registrieren Sie einen Kunden 'atapley2j@kinetecoinc.com' 
 -- für unsere Jubilaeumsfeier.
 -- Der Kunde wird mit drei Freunden an der Feier teilnehmen.
+
+Select KundenID from Kunden where Email = 'atapley2j@kinetecoinc.com';
+
+insert into TeilnahmeJubilaeum (KundenID, PartyGroesse)
+Values (
+  (Select KundenID
+  from Kunden 
+  where Email = 'atapley2j@kinetecoinc.com'),
+  3);
